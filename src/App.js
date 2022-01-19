fab38d0ab9e6a908fc8d82a2def5ac570b623fa3
Suzan Hamza <suzan.hamza@gmail.com>
2022-01-19 08:28:29 +0200

    Convert Classes to Functional Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import React, {useState} from 'react'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
+/** Convert Classes to Functional Components
+ * to be able to use Hooks
+ */
+const BooksApp = (props) => {
+  // state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
-  }
-
-  render() {
+  //   showSearchPage: false
+  // }
+  
+  const [showSearchPage, setShowSearchPage] = useState(false);
+  
+    console.log(BooksAPI.getAll());
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        {showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <button className="close-search" onClick={() => setShowSearchPage({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -194,13 +199,12 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+              <button onClick={() => setShowSearchPage({ showSearchPage: true })}>Add a book</button>
             </div>
           </div>
         )}
       </div>
     )
-  }
 }
 
 export default BooksApp
