c450fda61bc6a802fd1ff655c6f0468ea6ad1ef1
Suzan Hamza <suzan.hamza@gmail.com>
2022-01-19 20:40:50 +0200

    ListBook Component to list a single book

---
+++ src/ListBook.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+/** Component to list a single Book */
+const ListBook = (props) => {
+    // Get Required Book from Props
+    const book = props.book;
+    return (
+       <li key={book.id}>
+            <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-shelf-changer">
+                <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+                </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors.join(' - ')}</div>
+            </div>
+        </li>
+
+    )                    
+}
+export default ListBook
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,10 +1,9 @@
 import React from "react";
-
+import ListBook from "./ListBook";
 /** Component to list all Books */
 const ListBooks = (props) => {
     // Get Book List from Props
     const books = props.books;
-    console.log('Books::', books);
     return (
        <div className="list-books-content">
             <div>
@@ -14,24 +13,7 @@ const ListBooks = (props) => {
                 <ol className="books-grid">
                     {books.filter(book => book.shelf === "currentlyReading")
                     .map((book) => (
-                        <li key={book.id}>
-                            <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                <div className="book-shelf-changer">
-                                <select>
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
-                                </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
-                            </div>
-                        </li>
+                        <ListBook book={book} />
 
                     ))}
                 </ol>
@@ -43,25 +25,7 @@ const ListBooks = (props) => {
                 <ol className="books-grid">
                     {books.filter(book => book.shelf === "wantToRead")
                     .map((book) => (
-                        <li key={book.id}>
-                            <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                <div className="book-shelf-changer">
-                                <select>
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
-                                </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
-                            </div>
-                        </li>
-
+                        <ListBook book={book} />
                     ))}
                 </ol>
                 </div>
@@ -72,25 +36,7 @@ const ListBooks = (props) => {
                 <ol className="books-grid">
                     {books.filter(book => book.shelf === "read")
                     .map((book) => (
-                        <li key={book.id}>
-                            <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                <div className="book-shelf-changer">
-                                <select>
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
-                                </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
-                            </div>
-                        </li>
-
+                        <ListBook book={book} />
                     ))}
                 </ol>
                 </div>
